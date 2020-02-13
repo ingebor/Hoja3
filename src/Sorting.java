ad37035ab83f20187a18088023afdd5cea81073a
IngeborRV <47764062+ingebor@users.noreply.github.com>
2020-02-12 22:54:41 -0600

    Main

--- src/Sorting.java
+++ src/Sorting.java
@@ -20,22 +20,32 @@ public class Sorting {
     }
 
     //Code by Rajat Mishra
-    public void mergeSort(int arr[], int l, int m, int r){
-        int n1 = m-l+1;
-        int n2 = r-m;
+    void mergeSort(int arr[], int l, int m, int r)
+    {
+        // Find sizes of two subarrays to be merged
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        /* Create temp arrays */
         int L[] = new int [n1];
         int R[] = new int [n2];
-        for(int i= 0; i<n1;i++){
-            L[i] = arr[l+i];
-        }
-        for(int j=0; j<n2;j++){
-            L[j] = arr[m+1+j];
-        }
 
-        int i = 0, j=0;
+        /*Copy data to temp arrays*/
+        for (int i=0; i<n1; ++i)
+            L[i] = arr[l + i];
+        for (int j=0; j<n2; ++j)
+            R[j] = arr[m + 1+ j];
 
+
+        /* Merge the temp arrays */
+
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+
+        // Initial index of merged subarry array
         int k = l;
-        while (i<n1 &&j<n2){
+        while (i < n1 && j < n2)
+        {
             if (L[i] <= R[j])
             {
                 arr[k] = L[i];
@@ -48,17 +58,32 @@ public class Sorting {
             }
             k++;
         }
+
+        /* Copy remaining elements of L[] if any */
         while (i < n1)
         {
             arr[k] = L[i];
             i++;
             k++;
         }
+
+        /* Copy remaining elements of R[] if any */
         while (j < n2)
         {
             arr[k] = R[j];
             j++;
             k++;
+        }
+    }
+
+    //Code by Rajat Mishra
+    void sort(int arr[], int l, int r){
+        if (l < r)
+        {
+            int m = (l+r)/2;
+            sort(arr, l, m);
+            sort(arr , m+1, r);
+            mergeSort(arr, l, m, r);
         }
     }
 
@@ -90,6 +115,7 @@ public class Sorting {
         }
     }
 
+    //Code by Devesh Agrawal
     static int getMax(int arr[], int n){
         int mx = arr[0];
         for(int i=1;i<n;i++){
@@ -100,13 +126,14 @@ public class Sorting {
         return mx;
     }
 
+    //Code by Devesh Agrawal
     static void countSort(int arr[], int n, int exp){
         int output[] = new int[n];
         int i;
         int count[] = new int[10];
         Arrays.fill(count,0);
 
-        for(i=0 : i<n; i++){
+        for( i=0 ; i<n; i++){
             count[(arr[i]/exp)%10]++;
         }
         for(i=1;i<10;i++){
@@ -120,6 +147,8 @@ public class Sorting {
             arr[i] = output[i];
         }
     }
+
+    //Code by Devesh Agrawal
     static void radixSort(int arr[], int n){
         int m = getMax(arr,n);
         for(int exp=1; m/exp >0; exp *=10){
@@ -127,7 +156,9 @@ public class Sorting {
         }
     }
 
-    void sort(int arr[]){
+    //Code by Rajat Mishra
+    void insertionSort(int arr[]){
+        int n = arr.length;
         for (int i = 1; i<n;i++){
             int key = arr[i];
             int j = i -1;
---
+++ src/main.java
@@ -0,0 +1,44 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class main {
+    public Array lista;
+    public ArrayList listaFinal;
+    public static void main(String args[]){
+        ArrayList listaFinal = new ArrayList();
+
+        //3000 datos
+        int i = 0;
+        int lista[] = new int[3000];
+        while(i<3000){
+            int numero = (int) (Math.random()*i) +1;
+            lista[i]=numero;
+            i++;
+        }
+
+        System.out.println("esta es la lista generada: "+Arrays.toString(lista));
+
+        //******Quitar el comentario del que se desee probar******
+        Sorting ob = new Sorting();
+        //Selection sort
+        //ob.selectionSort(lista);
+
+       //Merge sort
+        //ob.sort(lista,0,lista.length-1);
+
+        //Quick sort
+        /*int n = lista.length;
+        ob.quickSort(lista,0,n-1);*/
+
+        //radix sort
+        /*int n = lista.length;
+        ob.radixSort(lista,n);*/
+
+        //Insertion sort
+        ob.insertionSort(lista);
+
+
+        System.out.println("Lista ordenada: "+Arrays.toString(lista));
+    }
+}
